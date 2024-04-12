87d25948bd806052a9e6bec395ca95f3378d1812
svelozs <soniavelozsegura@gmail.com>
2024-04-12 00:21:11 -0600

    Agregar roles

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,40 @@
+DROP TABLE IF EXISTS roles CASCADE;
+CREATE TABLE roles(
+	id BIGSERIAL PRIMARY KEY,
+	name name VARCHAR(180) NOT NULL UNIQUE,
+	image VARCHAR(255) NULL,
+	route VARCHAR(255) NULL,
+	created_at TIMESTAMP (0) NOT NULL,
+	updated_at TIMESTAMP (0) NOT NULL
+);
+
+INSERT INTO roles(
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'CLIENTE',
+	'client/services/list',
+	'2024-04-12',
+	'2024-04-12'
+);
+
+INSERT INTO roles(
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'SOCIO',
+	'socio/orders/list',
+	'2024-04-12',
+	'2024-04-12'
+);
+
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
 	id BIGSERIAL PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
@@ -9,4 +46,15 @@ CREATE TABLE users(
 	session_token VARCHAR (255) NULL,
 	created_at TIMESTAMP (0) NOT NULL,
 	updated_at TIMESTAMP (0) NOT NULL
+);
+
+DROP TABLE IF EXISTS user_has_roles CASCADE;
+CREATE TABLE user_has_roles(
+	id_user BIGSERIAL NOT NULL,
+	id_role BIGSERIAL NOT NULL,
+	created_at TIMESTAMP (0) NOT NULL,
+	updated_at TIMESTAMP (0) NOT NULL,
+	FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(id_role) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(id_user, id_role)
 );
